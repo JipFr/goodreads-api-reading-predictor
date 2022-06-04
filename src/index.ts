9b0e397568098069bf0ac2e03d7d6cb46a85d200
Jip Fr <jipfrijlink@gmail.com>
2022-06-04 23:49:09 +0200

    feat: create list viewer

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,10 @@
-import * as chalk from "chalk";
-const dayjs = require("dayjs");
-const relativeTime = require("dayjs/plugin/relativeTime");
+import chalk from "chalk";
 import { config } from "dotenv";
 config();
-
-dayjs.extend(relativeTime);
 
 import GoodReads from "./goodreads";
 
 (async () => {
-	// Initiate client
 	const gr = new GoodReads();
 
 	// Log in
@@ -18,98 +13,53 @@ import GoodReads from "./goodreads";
 
 	await gr.login(process.env.GR_USERNAME, process.env.GR_PASSWORD);
 
-	// Get current user data
-	const currentUser = gr.user;
-	if (currentUser) {
-		console.log(currentUser);
+	// Get all shelves
+	const shelves = gr.user?.user_shelves.user_shelf;
+	if (!shelves) throw new Error("NO_SHELVES");
+	if (!gr.user) throw new Error("NO_USER");
 
-		const readingShelf = await gr.getShelf(
-			gr?.user?.user.id,
-			"currently-reading"
-		);
-		const booksOnShelf = readingShelf.reviews.review;
-		console.log(JSON.stringify(booksOnShelf, null, "  "));
-
-		console.log("┌" + "─".repeat(38) + "┐", "\n");
-
-		for (let i = 0; i < booksOnShelf.length; i++) {
-			const book = booksOnShelf[i];
-			const updates = book.user_statuses.user_status;
-
-			// Log title
-			console.info(chalk.bold(book.book.title));
-
-			// Get predicted time until end of book
-			const timeUntilEndTotalInDays = Math.round(
-				getRemainingReadingTime(updates, updates.length) / (1e3 * 60 * 60 * 24)
+	async function getFullShelf(
+		userId: number,
+		shelfName: string,
+		page = 1
+	): Promise<any> {
+		const shelfRes = await gr.getShelf(gr.user?.user.id, shelfName, page, 20);
+		if (shelfRes.reviews.$.end !== shelfRes.reviews.$.total) {
+			// If not at the end of the pagination, get MOAR BOOKS!
+			shelfRes.reviews.review.push(
+				...(await getFullShelf(userId, shelfName, page + 1)).reviews.review
 			);
-
-			const timeUntilEndTotalInDaysBasedOnRecents = Math.round(
-				getRemainingReadingTime(updates, 5) / (1e3 * 60 * 60 * 24)
-			);
-
-			console.info(
-				`\nBased on your last ${chalk.bold(
-					updates.length
-				)} updates, it will take you about ${chalk.bold(
-					timeUntilEndTotalInDays
-				)} more days to finish this book.`
-			);
-
-			if (timeUntilEndTotalInDaysBasedOnRecents !== timeUntilEndTotalInDays) {
-				console.info(
-					`If we only look at your last ${chalk.bold(
-						5
-					)} updates, it will take you about ${chalk.bold(
-						timeUntilEndTotalInDaysBasedOnRecents
-					)} days`
-				);
-			}
-
-			// Log each update
-			for (const update of updates.slice(0, 5)) {
-				// Create progress bar
-				const block = "█";
-				const totalBlockCount = 35;
-				const updatePercentage = update.percent;
-				const completedBlocks = Math.round(
-					(updatePercentage / 100) * totalBlockCount
-				);
-				const bar =
-					chalk.green(block.repeat(completedBlocks)) +
-					chalk.bgBlack(block.repeat(totalBlockCount - completedBlocks));
-
-				// Create "x time ago"
-				const daysDiffFormatted = dayjs().to(update.updated_at);
-
-				// Log all the things
-				console.log("\n");
-				console.log(bar, updatePercentage + "%");
-				console.log(` ${daysDiffFormatted}`.padStart(totalBlockCount, "─"));
-			}
-
-			if (booksOnShelf[i + 1]) {
-				console.log("\n├" + "─".repeat(38) + "┤", "\n");
-			}
 		}
-		console.log("\n└" + "─".repeat(38) + "┘", "\n");
+		return shelfRes;
+	}
+
+	// Get all shelves in their entirety
+	const fullShelves = await Promise.all(
+		shelves.map(async (shelf) => {
+			if (!gr.user) throw new Error("NO_USER");
+			return await getFullShelf(gr.user?.user.id, shelf.name);
+		})
+	);
+
+	for (const shelf of fullShelves) {
+		const shelfName = shelf.shelf.$.name;
+		let formattedShelfName = shelfName.replace(/-/g, " ");
+		formattedShelfName =
+			formattedShelfName.slice(0, 1).toUpperCase() +
+			formattedShelfName.slice(1);
+		console.log(chalk.bold(formattedShelfName));
+		for (const book of shelf?.reviews?.review || []) {
+			console.log(
+				` - ${chalk.bold(
+					book.book.title_without_series
+				)} ${chalk.grey`- ${book.book.authors.author.name}`}`
+			);
+		}
+
+		if (!shelf?.reviews?.review) {
+			console.log(chalk.grey(" - No books in this shelf"));
+		}
+
+		console.log("\n");
 	}
 })();
-
-function getRemainingReadingTime(updates: any[], count = updates.length) {
-	const relevantUpdates = updates.slice(0, count);
-	const latestUpdate = relevantUpdates[0];
-	const earliestUpdate = relevantUpdates[relevantUpdates.length - 1];
-
-	const diff =
-		new Date().getTime() - new Date(earliestUpdate.updated_at).getTime();
-
-	const diffInPercentages = latestUpdate.percent - earliestUpdate.percent;
-
-	const remaining = 100 - latestUpdate.percent;
-
-	// Calculate how long it'll take to finish the book
-	const timeUntilEnd = diff * (remaining / diffInPercentages);
-
-	return timeUntilEnd;
-}
---
+++ src/predictor.ts
@@ -0,0 +1,115 @@
+import * as chalk from "chalk";
+const dayjs = require("dayjs");
+const relativeTime = require("dayjs/plugin/relativeTime");
+import { config } from "dotenv";
+config();
+
+dayjs.extend(relativeTime);
+
+import GoodReads from "./goodreads";
+
+(async () => {
+	// Initiate client
+	const gr = new GoodReads();
+
+	// Log in
+	if (!process.env.GR_USERNAME) throw new Error("GR_PROVIDE_USERNAME");
+	if (!process.env.GR_PASSWORD) throw new Error("GR_PROVIDE_PASSWORD");
+
+	await gr.login(process.env.GR_USERNAME, process.env.GR_PASSWORD);
+
+	// Get current user data
+	const currentUser = gr.user;
+	if (currentUser) {
+		console.log(currentUser);
+
+		const readingShelf = await gr.getShelf(
+			gr?.user?.user.id,
+			"currently-reading"
+		);
+		const booksOnShelf = readingShelf.reviews.review;
+		console.log(JSON.stringify(booksOnShelf, null, "  "));
+
+		console.log("┌" + "─".repeat(38) + "┐", "\n");
+
+		for (let i = 0; i < booksOnShelf.length; i++) {
+			const book = booksOnShelf[i];
+			const updates = book.user_statuses.user_status;
+
+			// Log title
+			console.info(chalk.bold(book.book.title));
+
+			// Get predicted time until end of book
+			const timeUntilEndTotalInDays = Math.round(
+				getRemainingReadingTime(updates, updates.length) / (1e3 * 60 * 60 * 24)
+			);
+
+			const timeUntilEndTotalInDaysBasedOnRecents = Math.round(
+				getRemainingReadingTime(updates, 5) / (1e3 * 60 * 60 * 24)
+			);
+
+			console.info(
+				`\nBased on your last ${chalk.bold(
+					updates.length
+				)} updates, it will take you about ${chalk.bold(
+					timeUntilEndTotalInDays
+				)} more days to finish this book.`
+			);
+
+			if (timeUntilEndTotalInDaysBasedOnRecents !== timeUntilEndTotalInDays) {
+				console.info(
+					`If we only look at your last ${chalk.bold(
+						5
+					)} updates, it will take you about ${chalk.bold(
+						timeUntilEndTotalInDaysBasedOnRecents
+					)} days`
+				);
+			}
+
+			// Log each update
+			for (const update of updates.slice(0, 5)) {
+				// Create progress bar
+				const block = "█";
+				const totalBlockCount = 35;
+				const updatePercentage = update.percent;
+				const completedBlocks = Math.round(
+					(updatePercentage / 100) * totalBlockCount
+				);
+				const bar =
+					chalk.green(block.repeat(completedBlocks)) +
+					chalk.bgBlack(block.repeat(totalBlockCount - completedBlocks));
+
+				// Create "x time ago"
+				const daysDiffFormatted = dayjs().to(update.updated_at);
+
+				// Log all the things
+				console.log("\n");
+				console.log(bar, updatePercentage + "%");
+				console.log(` ${daysDiffFormatted}`.padStart(totalBlockCount, "─"));
+			}
+
+			if (booksOnShelf[i + 1]) {
+				console.log("\n├" + "─".repeat(38) + "┤", "\n");
+			}
+		}
+		console.log("\n└" + "─".repeat(38) + "┘", "\n");
+	}
+})();
+
+function getRemainingReadingTime(updates: any[], count = updates.length) {
+	const relevantUpdates = updates.slice(0, count);
+	const latestUpdate = relevantUpdates[0];
+	const earliestUpdate = relevantUpdates[relevantUpdates.length - 1];
+
+	const diff =
+		new Date().getTime() - new Date(earliestUpdate.updated_at).getTime();
+
+	const diffInPercentages = latestUpdate.percent - earliestUpdate.percent;
+
+	const remaining = 100 - latestUpdate.percent;
+
+	// Calculate how long it'll take to finish the book
+	const timeUntilEnd = diff * (remaining / diffInPercentages);
+
+	return timeUntilEnd;
+}
